65d66c66f5d6507ccec24a797e4c3743d5309217
Madison Held <heldmadison1@gmail.com>
2026-07-15 15:37:16 -0700

    load required packages, copied GSEA gene-term spreadsheets into repository

--- MK22R-4_organ-GSEA.R
+++ MK22R-4_organ-GSEA.R
@@ -4,3 +4,46 @@
 # use the same Loupe cluster labeling that you used previously, 
 # just change clustering method to be whole organs instead of Loupe cluster numbers
 # this file is for one slide; explain that this is to be replicated for each slide
+
+
+# Install/load required packages
+
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("writexl")
+install.packages("utils")
+install.packages("Seurat")
+install.packages("Matrix")
+install.packages("hdf5r")
+install.packages("patchwork")
+install.packages("future")
+install.packages("sctransform")
+install.packages("arrow")
+install.packages("png")
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("EBImage")
+BiocManager::install("clusterProfiler")
+#BiocManager::install("celldex")
+#BiocManager::install("SingleR")
+BiocManager::install("org.Hs.eg.db")
+BiocManager::install("enrichplot")
+library(tidyverse)
+library(readxl)
+library(writexl)
+library(utils)
+library(Seurat)
+library(Matrix)
+library(hdf5r)
+library(patchwork)
+library(future)
+library(sctransform)
+library(arrow)
+library(png)
+library(EBImage)
+library(clusterProfiler)
+library(celldex)
+library(SingleR)
+library("org.Hs.eg.db", character.only = TRUE)
+library(enrichplot)
