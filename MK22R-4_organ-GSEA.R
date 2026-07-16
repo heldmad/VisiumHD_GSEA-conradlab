62191633ad6adfa15a37beaae7d51a18a23a1d6d
Madison Held <heldmadison1@gmail.com>
2026-07-15 20:18:08 -0700

    tidying script up

--- MK22R-4_organ-GSEA.R
+++ MK22R-4_organ-GSEA.R
@@ -1,11 +1,3 @@
-# try subsetting whole slide into whole organs and comparing those!
-# instead of comparing clusters within single organs
-# eg lungs vs all other organs, etc
-# use the same Loupe cluster labeling that you used previously, 
-# just change clustering method to be whole organs instead of Loupe cluster numbers
-# this file is for one slide; explain that this is to be replicated for each slide
-
-
 # Install/load required packages
 
 install.packages("tidyverse")
