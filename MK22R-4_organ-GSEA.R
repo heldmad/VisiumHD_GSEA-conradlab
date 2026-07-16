80a5b72e1bf7f1e0b2b771821a22f53a50274a94
Madison Held <heldmadison1@gmail.com>
2026-07-15 20:11:28 -0700

    wrote code for performing GSEA on lung (single organ) using data from Loupe browser and compiled birth defects gene lists

--- MK22R-4_organ-GSEA.R
+++ MK22R-4_organ-GSEA.R
@@ -12,70 +12,83 @@ install.packages("tidyverse")
 install.packages("readxl")
 install.packages("writexl")
 install.packages("utils")
-install.packages("Seurat")
-install.packages("Matrix")
-install.packages("hdf5r")
-install.packages("patchwork")
-install.packages("future")
-install.packages("sctransform")
-install.packages("arrow")
 install.packages("png")
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-
-BiocManager::install("EBImage")
 BiocManager::install("clusterProfiler")
-#BiocManager::install("celldex")
-#BiocManager::install("SingleR")
 BiocManager::install("org.Hs.eg.db")
 BiocManager::install("enrichplot")
 library(tidyverse)
 library(readxl)
 library(writexl)
 library(utils)
-library(Seurat)
-library(Matrix)
-library(hdf5r)
-library(patchwork)
-library(future)
-library(sctransform)
-library(arrow)
 library(png)
-library(EBImage)
 library(clusterProfiler)
-library(celldex)
-library(SingleR)
 library("org.Hs.eg.db", character.only = TRUE)
 library(enrichplot)
 
 
-# Loading in VisiumHD data (for Seurat purposes)
-localdir <- '~/Desktop/spatial_analysis/MK22R-4_combined_out/008um/binned_outputs/square_008um/'
-list.files(localdir)
-MK22R4_combined <- Load10X_Spatial(data.dir = localdir, 
-                                   filename = 'filtered_feature_bc_matrix.h5', 
-                                   assay = "spatial", 
-                                   slice = "slice1", 
-                                   filter.matrix = TRUE)
+# Create an object with ALL genes expressed in the UNCLUSTERED LUNG (DEGs in lung only regardless of cluster)
+# data from LOUPE BROWSER
+all_genes_wholelung <- read.csv("~/Desktop/scratchwork/MK22R-4_lungs_whole_DEGs.csv", header = TRUE)
+all_genes_wholelung <- all_genes_wholelung %>%
+  dplyr::select(FeatureName, lungs_QCd.Log2.Fold.Change, lungs_QCd.P.Value)
 
-# Set default assays to 'spatial'
-Assays(MK22R4_combined)
-DefaultAssay(MK22R4_combined) <- 'spatial'
+# Upload NEW term2gene lists from G2P and ClinGen
+clingen <- read_xlsx("~/Downloads/all_clingen_GCEPs_forR.xlsx")
+clingen <- clingen %>%
+  dplyr::select(GENE, TERM)
+G2P <- read_xlsx("~/Downloads/G2P_withlabels_forR.xlsx")
 
-# Visualize initial data
-vln.plot <- VlnPlot(MK22R4_combined, features = "nCount_spatial", pt.size = 0) + theme(axis.text = element_text(size = 4)) + NoLegend()
-count.plot <- SpatialFeaturePlot(MK22R4_combined, features = "nCount_spatial", image.scale = "hires") + theme(legend.position = "right") #image.scale = "hires"
-vln.plot | count.plot
+# Combine into one data frame and make a TERM2GENE list with gene as entrez IDs
+new_terms <- rbind(clingen, G2P)
+new_TERM2GENE_entrez <- select(org.Hs.eg.db,
+                               keys = new_terms$GENE,
+                               columns = c("ENTREZID", "ENSEMBL"),
+                               keytype = "SYMBOL")
+new_terms <- new_terms %>%
+  dplyr::select(TERM = TERM, SYMBOL = GENE)
+new_TERM2GENE <- left_join(new_terms, new_TERM2GENE_entrez, by = "SYMBOL")
+new_TERM2GENE <- new_TERM2GENE[!duplicated(new_TERM2GENE[,1:3]), ]
+new_TERM2GENE <- new_TERM2GENE %>%
+  dplyr::select(TERM = TERM, GENE = ENTREZID)
+new_TERM2GENE <- new_TERM2GENE %>%
+  filter(TERM != "NA")
 
-# Update the default image to the hires one (for easier visualization in the future)
-hires_image <- EBImage::readImage("~/Desktop/spatial_analysis/MK22R-4_combined_out/008um/spatial/tissue_hires_image.png")
-# Assign it to the image slot in your Seurat object
-DefaultAssay(MK22R4_combined) <- "spatial"
-MK22R4_combined@images$slice1@image <- hires_image
-MK22R4_combined@images$slice1@scale.factors
 
-# Normalize the data
-MK22R4 <- NormalizeData(MK22R4_combined)
-MK22R4 <- FindVariableFeatures(MK22R4)
-MK22R4 <- ScaleData(MK22R4)
-MK22R4 <- subset(MK22R4, subset = nCount_spatial > 0) #remove NA to do SCT transform
+## USING WHOLE LUNG DEGs 
+# filtering out high p-value and low L2FC genes
+
+#set seed
+set.seed(1)
+
+#create a vector for whole lung DEGs
+filtered_wholelung <- all_genes_wholelung %>%
+  filter(lungs_QCd.Log2.Fold.Change > 0.5 | lungs_QCd.Log2.Fold.Change < -0.5,
+         lungs_QCd.P.Value < 0.25)
+filtered_wholelung <- filtered_wholelung %>%
+  dplyr::select(SYMBOL = FeatureName, 
+                L2FC = lungs_QCd.Log2.Fold.Change, 
+                PVal_Adj = lungs_QCd.P.Value)
+filtered_wholelung_entrez <- select(org.Hs.eg.db,
+                                    keys = filtered_wholelung$SYMBOL,
+                                    columns = c("ENTREZID", "ENSEMBL"),
+                                    keytype = "SYMBOL")
+filtered_wholelunglist <- left_join(filtered_wholelung, filtered_wholelung_entrez, by = "SYMBOL")
+filtered_wholelunglist <- filtered_wholelunglist[!duplicated(filtered_wholelunglist[,1]), ]
+filtered_wholelunglist <- na.omit(filtered_wholelunglist)
+allgenesList_wlf <- filtered_wholelunglist[,2]
+names(allgenesList_wlf) <- as.character(filtered_wholelunglist[,4])
+allgenesList_wlf <- sort(allgenesList_wlf, decreasing = TRUE)
+
+wholelung_gsea <- GSEA(geneList = allgenesList_wlf,
+                       TERM2GENE = new_TERM2GENE,
+                       minGSSize = 1,
+                       maxGSSize = 7000,
+                       pvalueCutoff = 0.25,
+                       seed = TRUE,
+                       scoreType = "pos")
+readable_wholelung_gsea <- setReadable(wholelung_gsea, OrgDb = "org.Hs.eg.db", keyType = "ENTREZID")
+df_wholelung_gsea <- readable_wholelung_gsea@result
+gseaplot2(wholelung_gsea, df_wholelung_gsea[, 1])
+cnetplot(readable_wholelung_gsea)
